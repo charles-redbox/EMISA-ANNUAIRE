3db0ab2ab80b2f0d3d68c90efa08db1709d515c6
Charles <charles.arramon@redbox.fr>
2024-12-02 16:36:42 +0100

    implement login interface

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/message')]
+#[Route('/admin/message')]
 final class MessageController extends AbstractController
 {
     #[Route(name: 'app_message_index', methods: ['GET'])]
--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/student')]
+#[Route('/admin/student')]
 final class StudentController extends AbstractController
 {
     #[Route(name: 'app_student_index', methods: ['GET'])]
