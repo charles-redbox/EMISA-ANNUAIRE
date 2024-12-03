02691b94f0f32670d10fa5025e04a02e99fe71aa
Charles <charles.arramon@redbox.fr>
2024-12-03 17:00:12 +0100

    add features log controller

--- src/Controller/LogController.php
+++ src/Controller/LogController.php
@@ -22,26 +22,6 @@ final class LogController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_log_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $log = new Log();
-        $form = $this->createForm(LogType::class, $log);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($log);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_log_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('log/new.html.twig', [
-            'log' => $log,
-            'form' => $form,
-        ]);
-    }
-
     #[Route('/{id}', name: 'app_log_show', methods: ['GET'])]
     public function show(Log $log): Response
     {
@@ -50,32 +30,4 @@ final class LogController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_log_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Log $log, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(LogType::class, $log);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_log_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('log/edit.html.twig', [
-            'log' => $log,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_log_delete', methods: ['POST'])]
-    public function delete(Request $request, Log $log, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$log->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($log);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_log_index', [], Response::HTTP_SEE_OTHER);
-    }
 }
